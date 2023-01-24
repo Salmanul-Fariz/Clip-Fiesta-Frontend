0f4e27601cbf416c53fdd4fe7f7e8f6f8cbd281e
Salmanul Fariz N <salmanulfariz997@gmail.com>
2023-01-25 00:32:59 +0530

    created user auth componets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthComponent } from './user/auth/auth.component';
+import { FooterComponent } from './user/footer/footer.component';
+import { UserPagesComponent } from './user/user-pages/user-pages.component';
+import { HomeComponent } from './user/user-pages/home/home.component';
+import { SigninComponent } from './user/auth/signin/signin.component';
+import { SignupComponent } from './user/auth/signup/signup.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, AuthComponent, FooterComponent, UserPagesComponent, HomeComponent, SigninComponent, SignupComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
